a3d202290b11de8162ae92278b6b8f05225556c3
Marco <mbianchi@gmail.com>
2016-10-22 12:59:28 +0200

    Final version: empty strings check + message colors

--- module1-exercise/README.txt
+++ module1-exercise/README.txt
@@ -1,2 +1,4 @@
 Assignment 1
 
+- The empty strings between commas are not considered (cycles the array and checks for a not empty string, maybe this can be done with angular instead of js)
+- adds a class to the div in order to display the message in different colors
--- module1-exercise/app.js
+++ module1-exercise/app.js
@@ -16,12 +16,15 @@ function LunchCheckController($scope) {
     $scope.dishes.forEach($scope.countDishes);
     if($scope.numberOfDishes === 0) {
       $scope.message = "Please, enter data first.";
+      $scope.class = "red";
     }
     else if ($scope.numberOfDishes > 3) {
       $scope.message = "Too much!";
+      $scope.class = "green";
     }
     else if ($scope.numberOfDishes < 3){
       $scope.message = "Enjoy!";
+      $scope.class = "green";
     }
   };
   $scope.countDishes = function (item,index,arr) {
